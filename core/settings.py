49c0b68037df514e6d6651387e8733f4e79faaec
Nikhil-WebBuild <nikhilsuthar369@gmail.com>
2026-03-22 01:02:09 +0530

    create admin for postgres db

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'tasks'
+    'tasks',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
